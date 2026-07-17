5eb54339d7315880a88e8bcaf4ac09051b71d1c8
kiransoodyall03 <kiransoodyall03@gmail.com>
2026-07-18 00:06:02 +0200

    servicekey fix

--- src/backend/services/firebaseService.js
+++ src/backend/services/firebaseService.js
@@ -1,8 +1,8 @@
 const { initializeApp, cert } = require('firebase-admin/app');
 const { getFirestore } = require('firebase-admin/firestore');
 
-// Import your downloaded service account key
-const serviceAccount = require('../../../serviceAccountKey.json');
+// Parse the service account from the environment variable
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_KEY);
 
 // Initialize Firebase with your credentials using the modular 'cert' function
 initializeApp({
